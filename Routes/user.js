d10eac7eb0a0a4abb694210e9654f1d2dbffd099
ArthurHtbk <arthur.heurtebise@gmail.com>
2022-02-21 13:15:25 +0100

    fix typo

--- Routes/user.js
+++ Routes/user.js
@@ -3,7 +3,7 @@ const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
 const uid2 = require("uid2");
 
-const User = require("../models/User");
+const User = require("../Models/User");
 
 const router = express.Router();
 
